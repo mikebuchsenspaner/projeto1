d0e08c016440e64dd1591240bebc09ccf17ab2f3
Mike <mike.buchsenspaner@sou.inteli.edu.br>
2025-08-21 13:51:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from mean_var_std import calculate
 
 if __name__ == "__main__":
-    # Exemplo que o enunciado deu
-    example = [0,1,2,3,4,5,6,7,8]
+
+    example = [0,2,4,11,44,5,6,3,81]
     result = calculate(example)
     print("Resultado para [0..8]:")
     print(result)
